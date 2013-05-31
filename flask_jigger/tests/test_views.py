293ea2f2776c758dbf08b703f1b4edc50db89f43
RobVanMieghem <rob@awingu.com>
2013-05-31 10:06:27 +0200

    Wrap a target view in an ApiView object instead on only with a function

--- flask_jigger/tests/test_views.py
+++ flask_jigger/tests/test_views.py
@@ -1,19 +1,27 @@
 import unittest
+import mock
+import flask
+from flask_jigger.views import api_view
 
+class Test_Views(unittest.TestCase):
 
-class Test_api_view(unittest.TestCase):
-
-
-    def setUp(self):
-        pass
-
-
-    def tearDown(self):
-        pass
-
-
-    def testName(self):
-        pass
+    def test_api_view(self):
+        targetview = mock.MagicMock()
+        targetview.return_value = {'testkey':'testvalue'}
+        
+        renderer = mock.MagicMock()
+        renderer.render = mock.MagicMock()
+        renderer.render.return_value = 'ALL_OK'
+        renderer.media_type = 'application/testmediatype'
+        
+        app = flask.Flask(__name__)
+        app.add_url_rule("/test/<id>", 'test_function', api_view(targetview,[renderer]))
+            
+        with app.test_request_context('/test/123', headers={'Accept':'application/testmediatype'}):
+            response = app.full_dispatch_request()
+            self.assertEqual(response.data, 'ALL_OK')
+                
+        targetview.assert_called_once_with(id='123')
 
 
 if __name__ == "__main__":
--- flask_jigger/views.py
+++ flask_jigger/views.py
@@ -1,38 +1,53 @@
-from flask import request, abort
-from flask.globals import current_app
+from flask import request, abort, current_app
 import status
+from werkzeug import BaseResponse
 from renderers import JSONRenderer, JSONPRenderer, BrowsableAPIRenderer
 
 
 default_renderers = [JSONRenderer(), JSONPRenderer(), BrowsableAPIRenderer()]
 
-def api_view(view_function, renderers=default_renderers):
+
+class ApiView():
     '''
-    If 'renderers' is None, no special rendering will be done, the result of the function will just be passed to Flask to convert it a response.
+    Class for wrapping view_functions, use the api_view decorator to generate instances
     '''
     
-    def decorator(*args, **kwargs):
-        data = view_function(*args, **kwargs)
+    def __init__(self, view_function, renderers):
+        self.renderers = renderers
+        self.view_function = view_function
         
-        if renderers is None:
+    def __call__(self, *args, **kwargs):
+        data = self.view_function(*args, **kwargs)
+        
+        if self.renderers is None:
             return data;
         
         #Search the best renderer
-        media_type = request.accept_mimetypes.best_match([renderer.media_type for renderer in renderers], None)
+        media_type = request.accept_mimetypes.best_match([renderer.media_type for renderer in self.renderers], None)
         if media_type is None:
             abort(status.HTTP_406_NOT_ACCEPTABLE)
         
-        renderer = filter(lambda x:x.media_type == media_type,renderers)[0]
+        renderer = filter(lambda x:x.media_type == media_type,self.renderers)[0]
+        
+        #preliminary response class to pass to the renderer
+        beta_response = BaseResponse()
         
         #Render the result of the view function
         rendered_content = renderer.render(data, media_type, 
                                            {'request':request,
-                                            'view':view_function})
+                                            'response':beta_response,
+                                            'view':self.view_function,
+                                            'renderers':self.renderers})
         
         #Create a matching response
         response = current_app.response_class(
             rendered_content, mimetype=media_type)
         
         return response    
-            
-    return decorator
+
+def api_view(view_function, renderers=default_renderers):
+    '''
+    If 'renderers' is None, no special rendering will be done, the result of the function will just be passed to Flask to convert it a response.
+    '''
+    
+    return ApiView(view_function,renderers)
