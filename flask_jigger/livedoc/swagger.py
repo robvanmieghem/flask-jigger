723d5e2d3cba75a38901fa935fbf3a740df275ef
RobVanMieghem <rob@awingu.com>
2013-06-05 13:21:08 +0200

    Bassic Swagger support

--- flask_jigger/livedoc/swagger.py
+++ flask_jigger/livedoc/swagger.py
@@ -1,20 +1,111 @@
+from flask import request, current_app
+import flask_jigger.views
+import re
+
 class SwaggerDoc(object):
+    
+    def _generate_parameter(self, argument, rule):
+    
+        import werkzeug.routing
+        
+        if isinstance(rule._converters[argument],werkzeug.routing.FloatConverter):
+            datatype = "float"
+        else:
+            datatype = "string"
+        #TODO: more supported types
+                
+        parameter = {
+                    "paramType": "path",
+                    "name": argument,
+                    #TODO: "description": "blablabla",
+                    "dataType": datatype,
+                    "required": True,
+                    "allowMultiple": False
+                   }
+        
+        return parameter
+    
+    
+    def _description_from_name(self, view_function):
+        """
+        Translate 'CamelCaseNames' to 'Camel Case Names'.
+        Used when generating names from view classes.
+        """
+        name = view_function.__name__
+        camelcase_boundry = '(((?<=[a-z])[A-Z])|([A-Z](?![A-Z]|$)))'
+        content = re.sub(camelcase_boundry, ' \\1', name).strip()
+        return ' '.join(content.split('_')).title()
+    
+    
+    def _generate_apis(self,endpoint):
+        apis = []
+        
+        rules = current_app.url_map._rules_by_endpoint[endpoint]
+        view_target = current_app.view_functions[endpoint]
+        
+        if isinstance(view_target,flask_jigger.views.ApiView):
+            view_target = view_target.view_function
+        
+        view_function_docstring = view_target.__doc__
+        view_function_name = self._description_from_name(view_target)
 
-
-    def generate(self):
-        resourcelisting = {
-            'apiVersion': "0.2",
-            'swaggerVersion': "1.1",
-            'basePath': "http://petstore.swagger.wordnik.com/api",
-            'apis': [
-                     {
-                      'path': "/pet.{format}",
-                      'description': "Operations about pets"
-                      },
-                     {
-                      'path': "/user.{format}",
-                      'description': "Operations about user"
-                      }
-                     ]
-        }
-        return resourcelisting;
+        
+        for rule in rules:
+            tmp = []
+            for is_dynamic, data in rule._trace:
+                if is_dynamic:
+                    tmp.append('{%s}' % data)
+                else:
+                    tmp.append(data)
+            
+            path = u''.join(tmp).lstrip('|')
+        
+            api = {
+                  "path":path,
+                  "operations":[]
+                  }
+            
+            parameters = []
+            
+            for argument in rule.arguments:
+                parameters.append(self._generate_parameter(argument, rule))
+            
+            for method in rule.methods:
+                if method in ['GET','PUT', 'POST', 'DELETE']:
+                    operation = {
+                         "httpMethod":method,
+                         "nickname": method + endpoint,
+                         "parameters":parameters,
+                         "summary": view_function_name,
+                         "notes": view_function_docstring,
+                         "errorResponses":[]
+                         }
+            
+                    api['operations'].append(operation)
+                    
+            apis.append(api)
+         
+        return apis
+    
+    def generate_resource_description(self,endpoints):
+        '''
+        Create a swagger description for a list of endpoints
+        
+        Parameters
+        ----------
+        endpoints : list of str
+            The endpoints as passed to flask that need to be included in the api.
+        
+        '''
+        description = {
+                   "apiVersion": "0.2",
+                   "swaggerVersion": "1.1",
+                   "basePath": request.url_root,
+                   "resourcePath": request.path,
+                   "apis" : [],
+                   "models" : {}
+                   }
+        for endpoint in endpoints:
+            description['apis'].extend(self._generate_apis(endpoint))
+        
+        return description
--- flask_jigger/tests/livedoc/test_swaggerdoc.py
+++ flask_jigger/tests/livedoc/test_swaggerdoc.py
@@ -1,20 +1,67 @@
 import unittest
+import mock
+import flask
+from flask_jigger.views import api_view
+import json
 
 
 class Test_SwaggerDoc(unittest.TestCase):
 
 
     def setUp(self):
-        pass
-
-
+        self.targetview = mock.MagicMock()
+        self.targetview.__name__ = 'test_it'
+        self.targetview.return_value = {'testkey':'testvalue'}
+        
+        self.swaggerview = mock.MagicMock()
+        
+        self.app = flask.Flask(__name__)
+        self.app.add_url_rule("/swagger", 'swagger', api_view(self.swaggerview))
+        
     def tearDown(self):
         pass
 
+    def testGenerateApi(self):
+        self.app.add_url_rule("/test/<float:id>", 'test_function', api_view(self.targetview),methods=['GET'])
+        
+        def generateEndpointDoc():
+            from flask_jigger.livedoc import swagger
+            return swagger.SwaggerDoc().generate_resource_description(['test_function'])
+        
+        self.swaggerview.side_effect = generateEndpointDoc
+        
+        with self.app.test_request_context('/swagger', headers={'Accept':'application/json'}):
+            response = self.app.full_dispatch_request()
+            swaggerdescription = json.loads(response.data)
 
-    def testName(self):
-        pass
+        self.assertEqual(swaggerdescription['resourcePath'],'/swagger')
+        self.assertEqual(swaggerdescription['apis'][0]['path'], '/test/{id}')
+        
+    def testGenerateApi_Operation(self):
+        self.targetview.__doc__ = 'DocDescription'
+        
+        self.app.add_url_rule("/test/<float:id>", 'test_function', api_view(self.targetview),methods=['GET'])
+        
+        def generateEndpointDoc():
+            from flask_jigger.livedoc import swagger
+            return swagger.SwaggerDoc().generate_resource_description(['test_function'])
+        
+        self.swaggerview.side_effect = generateEndpointDoc
+        
+        with self.app.test_request_context('/swagger', headers={'Accept':'application/json'}):
+            response = self.app.full_dispatch_request()
+            swaggerdescription = json.loads(response.data)
 
+        operation = swaggerdescription['apis'][0]['operations'][0]
+        self.assertEqual(operation['httpMethod'], 'GET')
+        self.assertEqual(operation['notes'], "DocDescription")
+        self.assertEqual(operation['summary'],'Test It')
+        
+        parameter = operation['parameters'][0]
+        self.assertEqual(parameter['dataType'], 'float')
+        self.assertEqual(parameter['name'], 'id')
+        self.assertEqual(parameter['paramType'], 'path')
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
